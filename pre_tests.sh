eea3956bb406360fa546f7b3a7d970ff72e63eb5
Daniil Dulin <daniildulin@gmail.com>
2019-01-25 12:35:29 +0300

    update travis ci update make file

--- pre_tests.sh
+++ pre_tests.sh
@@ -6,4 +6,4 @@ CFG_STR2='", "port": "'
 CFG_STR3='" }, "gateApi": { "isSecure" : false, "link" : "", "port" : "9000" }, "wsServer":{ "isSecure" : false, "link" : "127.0.0.1", "port" : "8800", "key" : "secret key" } }'
 CFG="$CFG_STR1$GT_NODE_API_LINK$CFG_STR2$GT_NODE_API_PORT$CFG_STR3"
 
-echo -e "$CFG" > "config.json"
+echo -e "$CFG" > "$TRAVIS_HOME/gopath/src/github.com/daniildulin/explorer-gate/config.json"
