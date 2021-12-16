61cb13a442f28ec9d882ba246382e384d5fb7ef7
Daniil Dulin <daniildulin@gmail.com>
2021-12-16 10:49:27 +0300

    fix Estimate sell all coin

--- handlers/api_v2/estimate.go
+++ handlers/api_v2/estimate.go
@@ -206,62 +206,6 @@ func EstimateCoinSell(c *gin.Context) {
 	}
 }
 
-func EstimateCoinSellAll(c *gin.Context) {
-	gate, ok := c.MustGet("gate").(*core.MinterGate)
-	if !ok {
-		c.JSON(http.StatusRequestTimeout, gin.H{
-			"error": errors.NewGateError("Type cast error"),
-		})
-		return
-	}
-	coinToSell := strings.TrimSpace(c.Query(`coin_to_sell`))
-	coinToBuy := strings.TrimSpace(c.Query(`coin_to_buy`))
-	coinIdToSell := strings.TrimSpace(c.Query(`coin_id_to_sell`))
-	coinIdToBuy := strings.TrimSpace(c.Query(`coin_id_to_buy`))
-	gasPrice := strings.TrimSpace(c.Query(`gas_price`))
-	value := strings.TrimSpace(c.Query(`value_to_sell`))
-
-	swapFrom := strings.TrimSpace(c.Query(`swap_from`))
-	if swapFrom == "" {
-		swapFrom = "optimal"
-	}
-
-	var route []uint64
-	routes := c.QueryArray(`route`)
-	if len(routes) == 0 {
-		routes = c.QueryArray(`route[]`)
-	}
-	for _, r := range routes {
-		cId, err := strconv.ParseUint(r, 10, 64)
-		if err != nil {
-			gate.Logger.WithFields(logrus.Fields{
-				"coinToSell": coinToSell,
-				"coinToBuy":  coinToBuy,
-				"value":      value,
-			}).Warn(err)
-			errors.SetErrorResponse(err, c)
-			return
-		}
-		route = append(route, cId)
-	}
-
-	estimate, err := gate.EstimateCoinSellAll(coinToSell, coinIdToSell, coinToBuy, coinIdToBuy, value, gasPrice, swapFrom, route)
-	if err != nil {
-		gate.Logger.WithFields(logrus.Fields{
-			"coinToSell": coinToSell,
-			"coinToBuy":  coinToBuy,
-			"value":      value,
-		}).Warn(err)
-
-		errors.SetErrorResponse(err, c)
-	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"will_get":  estimate.Value,
-			"swap_from": estimate.SwapFrom,
-		})
-	}
-}
-
 func GetNonce(c *gin.Context) {
 	gate, ok := c.MustGet("gate").(*core.MinterGate)
 	if !ok {
